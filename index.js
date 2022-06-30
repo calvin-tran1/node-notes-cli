229e3ddddfd30210fef7cc9b497785ac7526f8f1
Ryan Bae <baeryan21@gmail.com>
2022-06-30 22:14:43 +0000

    switching

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const fs = require('fs');
 
 const dataJSON = require('./data.json');
 
-let id = fs.nextId;
-let notes = fs.notes;
+let id = dataJSON.nextId;
+let notes = dataJSON.notes;
 
+
+const operator = process.argv[2];
 const note = process.argv[3];
 // dataJSON.notes = note;
 const jsonFormat = JSON.stringify(dataJSON, null, 2);
-const operator = process.argv[2];
+const newEntryId = dataJSON.nextId;
+console.log(newEntryId)
 
-fs.writeFile('data.json', jsonFormat, err => {
+let newNoteEntry = dataJSON.notes.newEntryId;
+dataJSON.notes[`${newEntryId}`] = 'string'
+console.log(dataJSON.notes)
+
+fs.writeFile('./data.json', jsonFormat, err => {
   if (err) {
     console.error(err);
   }
   // console.log(process.argv[2]);
   if (operator === 'read') {
-    console.log(dataJSON);
+    console.log(dataJSON.notes);
+  } else if (operator === 'create' && note === dataJSON) {
+
   }
 })
