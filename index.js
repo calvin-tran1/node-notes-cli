54b81b0c1a992e67a6b84c2865a67670bc31a338
Calvin Tran <calvinxtran@gmail.com>
2022-06-30 19:25:05 +0000

    switching

---
+++ index.js
@@ -0,0 +1,11 @@
+// const fs = require('./data.json');
+
+// const note = process.argv[2] + '\n';
+
+// fs.writeFile('data.json', note, err => {
+//   if (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+
+// })
