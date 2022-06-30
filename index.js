a4f09f158c231fa97f3258a654124c3be75b2d7f
Calvin Tran <calvinxtran@gmail.com>
2022-06-30 22:31:20 +0000

    switching

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const fs = require('fs');
-
-const dataJSON = require('./data.json');
-
-let id = dataJSON.nextId;
-let notes = dataJSON.notes;
-
-
+const data = require('./data.json');
 const operator = process.argv[2];
 const note = process.argv[3];
-// dataJSON.notes = note;
-const jsonFormat = JSON.stringify(dataJSON, null, 2);
-const newEntryId = dataJSON.nextId;
-console.log(newEntryId)
+const jsonFormat = JSON.stringify(note, null, 2);
+let newEntryId = data.nextId;
+let notes = data.notes;
 
-let newNoteEntry = dataJSON.notes.newEntryId;
-dataJSON.notes[`${newEntryId}`] = 'string'
-console.log(dataJSON.notes)
+data.notes[`${newEntryId}`] = note;
 
-fs.writeFile('./data.json', jsonFormat, err => {
+
+// data.notes[`${newEntryId}`] = note;
+
+fs.writeFile('data.json', jsonFormat, err => {
   if (err) {
     console.error(err);
   }
   // console.log(process.argv[2]);
   if (operator === 'read') {
-    console.log(dataJSON.notes);
-  } else if (operator === 'create' && note === dataJSON) {
+    console.log(data.notes);
+  } else if (operator === 'create') {
 
+    data.nextId = newEntryId++;
   }
 })
+
+// console.log(data.notes);
