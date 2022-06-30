724ff91e07fed9b9d58498d4b5168c0a3f6b667c
Ryan Bae <baeryan21@gmail.com>
2022-06-30 23:00:26 +0000

    switching

--- index.js
+++ index.js
@@ -2,26 +2,46 @@ const fs = require('fs');
 const data = require('./data.json');
 const operator = process.argv[2];
 const note = process.argv[3];
-const jsonFormat = JSON.stringify(note, null, 2);
+// const jsonFormat = JSON.stringify(newEntry(), null, 2);
 let newEntryId = data.nextId;
 let notes = data.notes;
+const jsonFormat = JSON.stringify(data, null, 2);
 
-data.notes[`${newEntryId}`] = note;
 
+console.log(jsonFormat)
 
-// data.notes[`${newEntryId}`] = note;
+if (operator === 'read') {
+  console.log(data.notes);
+}
+if (operator === 'create') {
+  data.notes[`${newEntryId}`] = note;
+  fs.writeFile('data.json', jsonFormat, err => {
+    if (err) {
+      console.error(err);
+    }
+    })
+  data.nextId++
+}
 
-fs.writeFile('data.json', jsonFormat, err => {
-  if (err) {
-    console.error(err);
-  }
+console.log(notes)
+console.log(data)
+// fs.writeFile('data.json', jsonFormat, err => {
+//   if (err) {
+//     console.error(err);
+//   }
   // console.log(process.argv[2]);
-  if (operator === 'read') {
-    console.log(data.notes);
-  } else if (operator === 'create') {
+  // if (operator === 'read') {
+  //   console.log(data.notes);
+  // } else if (operator === 'create') {
 
-    data.nextId = newEntryId++;
-  }
+  //   data.nextId = newEntryId++;
+  // }
 })
 
+// if (operator === 'read') {
+//   console.log(data.notes);
+// } else if (operator === 'create') {
+
+//   data.nextId = newEntryId++;
+// }
 // console.log(data.notes);
